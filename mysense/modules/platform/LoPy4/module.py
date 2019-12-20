c149f894488b746547a122f30fca8a70e8d9a342
MarcoKull <marco.kull@posteo.de>
2019-12-20 11:37:51 +0100

    added configuration option for wifi access point

--- mysense/modules/platform/LoPy4/module.py
+++ mysense/modules/platform/LoPy4/module.py
@@ -18,6 +18,10 @@ class LoPy4(PlatformModule):
             p = Pin("P" + str(i),mode=Pin.OUT)
             p.value(1)
 
+        # set wifi access point
+        import pycom
+        pycom.wifi_on_boot(self.config().get("wifi_on_boot"))
+
     def is_run_tests(self):
         # only run test if not woke up from deep sleep
         import machine
@@ -31,6 +35,7 @@ class LoPy4(PlatformModule):
             "platform_lopy4",
             "LoPy4 platform options.",
             (
+                ("wifi_on_boot", "true", "If set to true a wireless access point will be started on boot.", ConfigFile.VariableType.bool),
                 ("pwr_pins", "", "A list of pins seperated by spaces which are used as power sources.", ConfigFile.VariableType.uint_list),
             )
         )
